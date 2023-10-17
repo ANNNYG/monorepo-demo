0d471d787fd5ed9a97b72a4401281ed7517faed7
Zyg <646124200@qq.com>
2023-10-17 11:28:29 +0800

    feat: 添加公共包

--- packages/decimal-demo/index.js
+++ packages/decimal-demo/index.js
@@ -1,4 +1,5 @@
 import Decimal from "decimal.js";
+import Dayjs from "dayjs";
 
 import { arr, result } from "lodash-demo";
 
@@ -6,6 +7,8 @@ const num_10 = new Decimal(10);
 const num_5 = new Decimal(5);
 
 const div_result = num_10.div(2);
+
+console.log(Dayjs());
 
 console.log(div_result, "div_result");
 console.log(arr, "result", result);
--- packages/lodash-demo/index.js
+++ packages/lodash-demo/index.js
@@ -1,6 +1,10 @@
 import _ from "lodash";
 
+import dayjs from "dayjs";
+
 const arr = ["a", "b", "c", "d"];
 const result = _.chunk(arr, 2);
+
+console.log(dayjs("2023-9-10").format("DD/MMMM/YYYY"), "====");
 
 export { result, arr };
